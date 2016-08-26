9a2d3d1246f1e2aeb536a63cf78d0096d6ce209b
shoen <shoen@DESKTOP-BB0D5VJ>
2016-08-26 13:49:54 -0400

    4th

--- FileLoader/FileLoader/FileLoaderModule/Attributes/AttributeController.cs
+++ FileLoader/FileLoader/FileLoaderModule/Attributes/AttributeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FileLoader.FileLoaderModule.Attribute
+namespace FileLoader.FileLoaderModule
 {
     class AttributeController
     {
